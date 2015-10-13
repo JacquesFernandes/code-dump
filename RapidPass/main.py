5204b425a0298f2a99ab6ab20bd58fb77433989a
JacuqesFernandes <jacques.fernandes.1337@gmail.com>
2015-10-13 08:04:02 +0530

    Added code

--- RapidPass/main.py
+++ RapidPass/main.py
@@ -10,14 +10,14 @@ f = Key("f","rvdg");
 g = Key("g","tbfh");
 h = Key("h","yngj");
 i = Key("i","8kuo");
-j
-k
-l
-m
-n
-o
-p
-q
+j = Key("j","umhk");
+k = Key("k","i-jl");
+l = Key("l","o-k-");
+m = Key("m","j-n-");
+n = Key("n","h-bm");
+o = Key("o","9lip");
+p = Key("p","0-o-");
+q = Key("q"
 r
 s
 t
